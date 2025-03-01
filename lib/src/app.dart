f15458d75b8277753adb182e921b7611c101ae7f
chard <dev.richardpesino@gmail.com>
2025-03-01 15:45:19 +0800

    fixed imports and deleted unused files

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:my_accountant/src/features/home/view/home_screen.dart';
+import 'package:my_accountant/src/controller/settings_controller.dart';
+import 'package:my_accountant/src/features/home/home_screen.dart';
 import 'package:my_accountant/src/util/theme/theme.dart';
-
-import 'settings/settings_controller.dart';
-import 'settings/settings_view.dart';
+import 'package:my_accountant/src/features/settings/settings_view.dart';
 
 /// The Widget that configures your application.
 class MyApp extends StatelessWidget {
